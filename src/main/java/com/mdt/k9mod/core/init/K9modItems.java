7c3379e1762a7f81255c727f05ea9d604a690d35
Loqor <65447472+Loqor@users.noreply.github.com>
2023-02-05 15:52:23 -0600

    added recipe for the metallic bone and also made sure the wrench and bone only stack to 1

--- src/main/java/com/mdt/k9mod/core/init/K9modItems.java
+++ src/main/java/com/mdt/k9mod/core/init/K9modItems.java
@@ -13,7 +13,7 @@ public class K9modItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, K9Mod.MOD_ID);
 
     public static final RegistryObject<Item> K9_WRENCH = ITEMS.register(
-            "k9_wrench", () -> new Item(new Item.Properties().defaultDurability(16).tab(ItemGroup.TAB_REDSTONE)));
+            "k9_wrench", () -> new Item(new Item.Properties().defaultDurability(16).tab(ItemGroup.TAB_REDSTONE).stacksTo(1)));
     public static final RegistryObject<Item> K9_BONE = ITEMS.register(
-            "k9_bone", () -> new BoneItem(new Item.Properties().defaultDurability(8).tab(ItemGroup.TAB_REDSTONE)));
+            "k9_bone", () -> new BoneItem(new Item.Properties().defaultDurability(8).tab(ItemGroup.TAB_REDSTONE).stacksTo(1)));
 }
